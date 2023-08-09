a16174014d340591426f828f58a59d1f7ef92787
Kevin <kevinyeh30000@gmail.com>
2023-08-09 17:34:14 +0800

    new

--- 7_21-testing project/00_Pure Pursuit Algorithm_ver_葉雅堂_checkpoint2_fixed/beta/helperGetReferencePath.m
+++
@@ -1,8 +0,0 @@
-function refPathOut = helperGetReferencePath()
-persistent refPath
-if isempty(refPath)
-    waypoints = [0 50; 150 50; 300 75; 310 75; 400 0; 300 -50; 290 -50; 0 -50]; % in meters
-    refPath = referencePathFrenet(waypoints);
-end
-refPathOut = refPath;
-end
